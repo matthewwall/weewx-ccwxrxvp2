921f32aabd900ec2e4592ddf5d753592f934737e
fergusd <fergusd@users.noreply.github.com>
2015-07-26 23:24:54 +0100

    add packet decode to mystick

--- mystick.py
+++ mystick.py
@@ -9,16 +9,85 @@ import time
 import serial
 import threading
 
+VP2P_UV       = 0x4 # UV index
+VP2P_RAINSECS = 0x5 # seconds between rain bucket tips
+VP2P_SOLAR    = 0x6 # solar irradiation
+VP2P_TEMP     = 0x8 # outside temperature
+VP2P_WINDGUST = 0x9 # 10-minute wind gust
+VP2P_HUMIDITY = 0xA # outside humidity
+VP2P_RAIN     = 0xE # rain bucket tips counter
+
 def processIstring(string) :
+    print "ISS packet : ", string
     fields = string.strip().split()
-    byte0 = int(fields[2],16)
-    byte1 = int(fields[3],16)
-    byte2 = int(fields[4],16)
-    byte3 = int(fields[5],16)
-    byte4 = int(fields[6],16)
-    byte5 = int(fields[7],16)
-    byte6 = int(fields[8],16)
-    byte7 = int(fields[9],16)
+    try :
+        byte0 = int(fields[2],16)
+        byte1 = int(fields[3],16)
+        byte2 = int(fields[4],16)
+        byte3 = int(fields[5],16)
+        byte4 = int(fields[6],16)
+        byte5 = int(fields[7],16)
+        byte6 = int(fields[8],16)
+        byte7 = int(fields[9],16)
+    except :
+        print "Invalid Packet"
+        return;
+
+    # checksum is verified by the monteino firmware
+    # checking it here is a little OTT
+
+    transmitterId = byte0 & 0x07
+    print "Transmitter ID : ", transmitterId
+
+    batteryStatus = (byte0 & 0x08) >> 3;
+    print "Battery Status : ", batteryStatus
+
+    windSpeed = byte1;
+    windDirection = (byte2 * 360) / 255;
+    print "Wind : ", windSpeed, "@", windDirection
+
+    packetType = byte0 >> 4
+
+    if packetType == VP2P_UV :
+        print "VP2P_UV"
+    elif packetType == VP2P_RAINSECS :
+        print"VP2P_RAINSECS"
+        if byte3 == 0xff :
+            # no rain
+            print "No Rain"
+            rainRate = 0
+        elif (byte4 & 0x40) == 0 :
+            # light rain
+            # 720 / (((Byte4 && 0x30) / 16 * 250) + Byte3)
+            rainRate = 720 / (((byte4 & 0x30) / 16 * 250) + byte3)
+        elif (byte4 & 0x40) == 0x40 :
+            # strong rain
+            # 11520 / (((Byte4 && 0x30) / 16 * 250) + Byte3)
+            rainRate = 11520 / (((byte4 & 0x30) / 16 * 250) + byte3)
+        print "Rain Rate : ",rainRate
+    elif packetType == VP2P_SOLAR :
+        print"VP2P_SOLAR"
+    elif packetType == VP2P_TEMP :
+        print"VP2P_TEMP"
+        externalTemperature10xF = (((byte3 << 8) + (byte4)) >> 4);
+        externalTemperatureF = float(externalTemperature10xF) / 10
+        externalTemperature10xC = (((externalTemperature10xF-320)*5)/9);
+        externalTemperatureC = float(externalTemperature10xC) / 10
+        print "Temp : ",externalTemperatureC,"C(",externalTemperatureF,"F)"
+    elif packetType == VP2P_WINDGUST :
+        print"VP2P_WINDGUST"
+        windGust = byte3;
+        print "Wind Gust : ", windGust
+    elif packetType == VP2P_HUMIDITY :
+        print"VP2P_HUMIDITY"
+        externalHumidity = (((byte4 & 0xf0) << 4) + byte3) / 10
+        print "Humidity : ",externalHumidity,"%"
+    elif packetType == VP2P_RAIN :
+        print"VP2P_RAIN"
+	rainTicks = byte3;
+        print "Rain Ticks : ", rainTicks
+    else :
+        print "Unknown Packet Type"
 
 def processBstring(string) :
     fields = string.strip().split()
@@ -34,6 +103,7 @@ ser = serial.Serial(
 def getSerialData(lock) :
     ser.open()
     ser.isOpen()
+    ser.flushInput()
 
     string = ''
     started = False
@@ -47,7 +117,7 @@ def getSerialData(lock) :
 		    string += data
 	    else :
             	if (data == '\r') :
-		    print string
+#		    print string
 		    started = False
 		    if string[0] == 'I' :
 			processIstring(string)
